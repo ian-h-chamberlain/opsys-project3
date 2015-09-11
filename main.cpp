3d44bf43cd434ec94725dd34901194e6d7d1a888
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2015-09-11 15:18:04 -0400

    Split out files, included processes.txt

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <fstream>
-#include <deque>
 #include <cstdlib>
 #include <string>
+#include <deque>
 #include <cstdio>
 
 #include "process.h"
 
 void readFile(const std::string &filename, std::deque<Process> &processes);
 int simulate(const std::deque<Process> &processes);
-void printQueue(const std::deque<Process> &queueToPrint);
 
 /**
  * Operating Systems Project 1
@@ -29,74 +28,10 @@ int main (int argc, char* argv[]) {
     // read the input file and populate the deque
     readFile(argv[1], processes);
 
-    std::cout << "time " << simulate(processes) << "ms: Simulator ended" << std::endl;
+    int finalTime = simulate(processes);
+    std::cout << "time " << finalTime << "ms: Simulator ended" << std::endl;
 
     return EXIT_SUCCESS;
-}
-
-/**
- * simulate: Simulate processes based on the queue passed in
- */
-int simulate(const std::deque<Process> &processes) {
-    std::deque<Process> execQueue(processes);
-    std::deque<Process> ioQueue;
-
-    // number of processes
-
-    // context-switch time delay (ms)
-    int t_cs = 13;
-
-    // initial cpu time is zero
-    int t = 0;
-    std::cout << "time " << t << "ms: Simulator started ";
-    printQueue(execQueue);
-
-    // run process until we run out of processes in either queue
-    while (execQueue.size() != 0) { // || ioQueue.size() != 0) {
-        // add time for context switch
-        t += t_cs;
-
-        if (execQueue.size() != 0) {
-            // accept the new process
-            Process curProc = execQueue.front();
-            execQueue.pop_front();
-
-            // print that it is beginning 
-            std::cout << "time " << t << "ms: P" << curProc.getNum()
-                << " started using the CPU ";
-            printQueue(execQueue);
-
-            // and change the clock time and number of bursts it needs
-            t += curProc.getBurstTime();
-            curProc.runBurst();
-            std::cout << "time " << t << "ms: P" << curProc.getNum()
-                << " completed its CPU burst ";
-            printQueue(execQueue);
-
-            // add to the ioQueue once process completes its burst
-            if (!curProc.isComplete()) {
-                ioQueue.push_back(curProc);
-            }
-        }
-    }
-
-    return t;
-}
-
-/**
- *  printQueue: a helper function to print the process queue
- */
-void printQueue(const std::deque<Process> &queueToPrint) {
-    if (queueToPrint.size() == 0) {
-        std::cout << std::endl;
-        return;
-    }
-    // print [Q 1 2 ... ]
-    std::cout << "[Q ";
-    for (unsigned int i=0; i<queueToPrint.size() - 1; i++) {
-        std::cout << queueToPrint[i].getNum() << " ";
-    }
-    std::cout << queueToPrint[queueToPrint.size() - 1].getNum() << "]" << std::endl;
 }
 
 /**
---
+++ simulation.cpp
@@ -0,0 +1,73 @@
+#include <deque>
+#include <iostream>
+
+#include "process.h"
+
+void printQueue(const std::deque<Process> &queueToPrint);
+
+/**
+ * simulate: Simulate processes based on the queue passed in
+ */
+int simulate(const std::deque<Process> &processes) {
+    std::deque<Process> execQueue(processes);
+    std::deque<Process> ioQueue;
+
+    // number of processes
+
+    // context-switch time delay (ms)
+    int t_cs = 13;
+
+    // initial cpu time is zero
+    int t = 0;
+    std::cout << "time " << t << "ms: Simulator started ";
+    printQueue(execQueue);
+
+    // run process until we run out of processes in either queue
+    while (execQueue.size() != 0) { // || ioQueue.size() != 0) {
+        // add time for context switch
+        t += t_cs;
+
+        if (execQueue.size() != 0) {
+            // accept the new process
+            Process curProc = execQueue.front();
+            execQueue.pop_front();
+
+            // print that it is beginning 
+            std::cout << "time " << t << "ms: P" << curProc.getNum()
+                << " started using the CPU ";
+            printQueue(execQueue);
+
+            // and change the clock time and number of bursts it needs
+            t += curProc.getBurstTime();
+            curProc.runBurst();
+            std::cout << "time " << t << "ms: P" << curProc.getNum()
+                << " completed its CPU burst ";
+            printQueue(execQueue);
+
+            // add to the ioQueue once process completes its burst
+            if (!curProc.isComplete()) {
+                ioQueue.push_back(curProc);
+            }
+        }
+    }
+
+    // return the final time in ms
+    return t;
+}
+
+/**
+ *  printQueue: a helper function to print the process queue
+ */
+void printQueue(const std::deque<Process> &queueToPrint) {
+    // if we have size zero, then just print "[Q]"
+    if (queueToPrint.size() == 0) {
+        std::cout << "[Q]" << std::endl;
+        return;
+    }
+    // print "[Q 1 2 ... ]"
+    std::cout << "[Q ";
+    for (unsigned int i=0; i<queueToPrint.size() - 1; i++) {
+        std::cout << queueToPrint[i].getNum() << " ";
+    }
+    std::cout << queueToPrint[queueToPrint.size() - 1].getNum() << "]" << std::endl;
+}
